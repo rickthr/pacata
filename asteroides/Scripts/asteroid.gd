cadbd456789841a894201fc4012bb8ed50b7b9d8
PEDRO LUCAS MORAIS MATOS <pedrolmatos.dev@gmail.com>
2026-06-07 13:36:32 -0300

    Asteroide destruição com sprite(parte 1)

--- asteroides/Scripts/asteroid.gd
+++ asteroides/Scripts/asteroid.gd
@@ -1,7 +1,7 @@
 # asteroid.gd
 class_name Asteroid
 extends AsteroidBase
-@onready var particles: GPUParticles2D = $DeathParticles
+
 @export var fragment_scene: PackedScene
 @export var fragment_textures: Array[Texture2D]
 
@@ -16,12 +16,9 @@ func setup(asteroide_dict: Dictionary, minerio_dict: Dictionary) -> void:
 	
 #Função destruir com particular sendo emitidas
 func _ao_destruir() -> void:
-	particles.emitting = true
-	particles.reparent(get_parent())
-	destruido.emit(_montar_dados()) 
-	queue_free()                       
-	await get_tree().create_timer(particles.lifetime).timeout
-	particles.queue_free() 
+	_lancar_fragmentos()
+	destruido.emit(_montar_dados())
+	queue_free()
 
 #função cores do minerio por raridade(não está funcionando tão bem)
 func _aplicar_vfx() -> void:
@@ -29,16 +26,11 @@ func _aplicar_vfx() -> void:
 	match raridade:
 		"Comum":
 			sprite.modulate = Color(1.0, 1.0, 1.0)
-			particles.modulate = Color(1.0, 1.0, 1.0)
-
 		"Raro":
 			sprite.modulate = Color(0.55, 0.20, 0.80)
-			particles.modulate = Color(1.0, 1.0, 1.0)
 			rot_speed *= 1.4
-
 		"Epico":
 			sprite.modulate = Color(0.898, 0.02, 0.2, 1.0)
-			particles.modulate = Color(0.898, 0.02, 0.2, 1.0)
 			rot_speed *= 2.0
 			_is_epico   = true
 			_base_scale = sprite.scale if sprite else Vector2.ONE
@@ -50,18 +42,18 @@ func _physics_process(delta: float) -> void:
 		_pulse_time += delta * 2.5
 		sprite.scale = _base_scale * (1.0 + sin(_pulse_time) * 0.07)
 		
-		
-		
 func _lancar_fragmentos() -> void:
 	if not fragment_scene or fragment_textures.is_empty():
 		return
-	for textura in fragment_textures:
+	
+	var total := fragment_textures.size()
+	for i in total:
 		var frag = fragment_scene.instantiate()
 		get_parent().add_child(frag)
 		frag.position = position
-		frag.setup(textura, sprite.modulate)
-		var direcao := Vector2(
-			randf_range(-1.0, 1.0),
-			randf_range(-1.0, 1.0)
-		).normalized()
-		frag.apply_impulse(direcao * randf_range(80.0, 200.0))
+		frag.setup(fragment_textures[i], sprite.modulate)
+		
+		var angulo := (TAU / total) * i + randf_range(-0.3, 0.3)
+		var forca  := randf_range(60.0, 140.0)
+		var direcao := Vector2(cos(angulo), sin(angulo))
+		frag.apply_impulse(direcao * forca)
--- asteroides/Scripts/fragment.gd
+++ asteroides/Scripts/fragment.gd
@@ -1,4 +1,5 @@
-extends Node2D
+extends RigidBody2D
+
 @onready var sprite: Sprite2D = $Sprite2D
 
 var lifetime: float = 0.8
@@ -8,5 +9,8 @@ func setup(textura: Texture2D, cor: Color) -> void:
 	sprite.modulate = cor
 
 func _ready() -> void:
+	gravity_scale   = 0.15   
+	linear_damp     = 1.5  
+	angular_velocity = randf_range(-5.0, 5.0) 
 	await get_tree().create_timer(lifetime).timeout
 	queue_free()
